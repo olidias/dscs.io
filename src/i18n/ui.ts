00c215563e79052aa99cc0bd955ecc96b4d531c7
Oli Dias <oli.dias94@proton.me>
2023-04-23 18:23:09 +0200

    initial commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+    en: 'English',
+    de: 'Deutsch'
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'header.title': 'Dias Software Consulting Services',
+    'header.subtitle': 'Solid Software. Competent Consulting.'
+  },
+  de: {
+
+  }
+} as const;
+
